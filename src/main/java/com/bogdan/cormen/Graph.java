3c0e3982e8002037e3b5fa78af138745ade7a83a
Bogdan Amihaesie <bogdan.am6@gmail.com>
2011-10-07 09:55:42 -0700

    first commit

---
+++ src/main/java/com/bogdan/cormen/Graph.java
@@ -0,0 +1,12 @@
+package com.bogdan.cormen;
+
+/**
+ * @author bogdan
+ */
+public class Graph {
+
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+
+}
---
+++ src/test/java/com/bogdan/cormen/GraphTest.java
@@ -0,0 +1,16 @@
+package com.bogdan.cormen;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class GraphTest {
+
+    @Test
+    public void testGraph() {
+        assertTrue(true);
+    }
+}
