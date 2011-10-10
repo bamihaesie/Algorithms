89747bfb85c54240a90a96a3b60f093903613179
Bogdan Amihaesie <bogdan.am6@gmail.com>
2011-10-10 16:57:55 +0100

    added graph boilerplate

--- src/main/java/com/bogdan/cormen/Graph.java
+++ src/main/java/com/bogdan/cormen/Graph.java
@@ -1,12 +1,117 @@
 package com.bogdan.cormen;
 
+import sun.net.idn.StringPrep;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static junit.framework.Assert.fail;
+
 /**
  * @author bogdan
  */
 public class Graph {
 
-    public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+    private boolean directed;
+    public List<Integer> V;
+    public Map<Integer, List<Integer>> E;
+
+    public Graph(File inputFile, boolean directed) {
+        this.directed = directed;
+        try {
+            buildFromFile(inputFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<Integer> getAdjListForNode(Integer id) {
+        return E.get(id);
+    }
+
+    public boolean isEdge(Integer u, Integer v) {
+        List<Integer> adjListForU = getAdjListForNode(u);
+        if (adjListForU.contains(v)) {
+            return true;
+        }
+        return false;
+    }
+
+    public void buildFromFile(File inputFile) throws IOException {
+
+        int numberOfVertices;
+        BufferedReader br = new BufferedReader(new FileReader(inputFile));
+
+        String line = br.readLine();
+        numberOfVertices = Integer.parseInt(line.trim());
+
+        V = new ArrayList<Integer>();
+        V.add(null);
+        for (int i=1; i <= numberOfVertices; i++) {
+            V.add(new Integer(i));
+        }
+
+        E = new HashMap<Integer, List<Integer>>();
+        while ((line = br.readLine()) != null ) {
+            String[] tokens = line.split(" ");
+            Integer u = Integer.valueOf(tokens[0]);
+            Integer v = Integer.valueOf(tokens[1]);
+
+            if (E.containsKey(V.get(u))) {
+                E.get(V.get(u)).add(v);
+            } else {
+                List<Integer> adjList = new ArrayList<Integer>();
+                adjList.add(v);
+                E.put(u, adjList);
+            }
+
+            if (directed == false) {
+                if (E.containsKey(V.get(v))) {
+                    E.get(V.get(v)).add(u);
+                } else {
+                    List<Integer> adjList = new ArrayList<Integer>();
+                    adjList.add(u);
+                    E.put(v, adjList);
+                }
+            }
+        }
+
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("V = [");
+        if (V != null) {
+            for (Integer v : V) {
+                if (v != null) {
+                    sb.append(v);
+                    if (v != V.size() - 1) {
+                        sb.append(", ");
+                    }
+                }
+            }
+        }
+        sb.append("]\n");
+
+        sb.append("E = [");
+        if (E != null) {
+            for (Integer key : E.keySet()) {
+                List<Integer> adjList = E.get(key);
+                for (Integer adj : adjList) {
+                    sb.append(key + "->" + adj);
+                    sb.append(", ");
+                }
+            }
+        }
+        sb.deleteCharAt(sb.length()-1);
+        sb.deleteCharAt(sb.length()-1);
+        sb.append("]");
+
+        return sb.toString();
     }
 
 }
--- src/test/java/com/bogdan/cormen/GraphTest.java
+++ src/test/java/com/bogdan/cormen/GraphTest.java
@@ -1,16 +1,67 @@
 package com.bogdan.cormen;
 
+import org.junit.Before;
 import org.junit.Test;
 
-import static junit.framework.Assert.assertTrue;
+import java.io.*;
+import java.net.URL;
+import java.util.List;
+
+import static junit.framework.Assert.*;
 
 /**
- * Unit test for simple App.
+ * Unit test for Graph
  */
 public class GraphTest {
 
+    File graphFile1 = null;
+
+    @Before
+    public void setUp() {
+        URL url = this.getClass().getResource("/graph1.txt");
+        assertNotNull("Can't open input file!", url);
+        graphFile1 = new File(url.getFile());
+    }
+
     @Test
-    public void testGraph() {
-        assertTrue(true);
+    public void testBuildUndirectedGraphFromFile() {
+        Graph g = new Graph(graphFile1, false);
+        assertNotNull(g);
+
+        assertNotNull(g.V);
+        assertEquals(g.V.size(), 1+5);
+        assertTrue(g.V.get(0) == null);
+        for (int i=1; i<=5; i++) {
+            assertEquals(new Integer(i), g.V.get(i));
+        }
+
+        assertNotNull(g.E);
+        assertEquals(5, g.E.size());
+        int[] expectedSizes = { -1, 2, 4, 2, 3, 3 };
+        for (int i=1; i<=5; i++) {
+            List<Integer> adjList = g.E.get(i);
+            assertEquals(expectedSizes[i], adjList.size());
+        }
+    }
+
+    @Test
+    public void testBuildDirectedGraphFromFile() {
+        Graph g = new Graph(graphFile1, true);
+        assertNotNull(g);
+
+        assertNotNull(g.V);
+        assertEquals(g.V.size(), 1+5);
+        assertTrue(g.V.get(0) == null);
+        for (int i=1; i<=5; i++) {
+            assertEquals(new Integer(i), g.V.get(i));
+        }
+
+        assertNotNull(g.E);
+        assertEquals(4, g.E.size());
+        int[] expectedSizes = { -1, 2, 3, 1, 1 };
+        for (int i=1; i<=4; i++) {
+            List<Integer> adjList = g.E.get(i);
+            assertEquals(expectedSizes[i], adjList.size());
+        }
     }
 }
