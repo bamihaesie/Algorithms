77222ead1f37b7d9e747c8488742b62e3ba3239c
Bogdan Amihaesie <bogdan.am6@gmail.com>
2012-01-28 17:35:51 +0000

    moved node class in a separate file

---
+++ src/main/java/com/bogdan/cormen/graphs/Node.java
@@ -0,0 +1,11 @@
+package com.bogdan.cormen.graphs;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: bogdan
+ * Date: 28/01/12
+ * Time: 17:32
+ * To change this template use File | Settings | File Templates.
+ */
+public class Node {
+}
