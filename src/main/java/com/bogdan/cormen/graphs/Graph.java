1fe02858ed79c7e0b6b4454c339beb0e8497088d
Bogdan Amihaesie <bogdan.am6@gmail.com>
2012-01-28 17:36:20 +0000

    moved node class in a separate file

--- src/main/java/com/bogdan/cormen/graphs/Graph.java
+++ src/main/java/com/bogdan/cormen/graphs/Graph.java
@@ -142,50 +142,5 @@ public class Graph {
             }
             u.color = NodeColor.BLACK;
         }
-
-    }
-
-}
-
-class Node implements Comparable<Node>{
-    Integer id;
-    NodeColor color;
-    Integer distance;
-    Node parent;
-
-    public Node(Integer id) {
-        this.id = id;
-    }
-
-    public int compareTo(Node otherNode) {
-        return this.id.compareTo(otherNode.id);
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        if (object instanceof Node) {
-            Node otherNode = (Node)object;
-            if (this.id == otherNode.id) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    @Override
-    public int hashCode() {
-        return id;
-    }
-
-    @Override
-    public String toString() {
-        return "" + id;
     }
 }
-
-enum NodeColor {
-    WHITE,
-    GREY,
-    BLACK
-}
-
--- src/main/java/com/bogdan/cormen/graphs/Node.java
+++ src/main/java/com/bogdan/cormen/graphs/Node.java
@@ -1,11 +1,43 @@
 package com.bogdan.cormen.graphs;
 
-/**
- * Created by IntelliJ IDEA.
- * User: bogdan
- * Date: 28/01/12
- * Time: 17:32
- * To change this template use File | Settings | File Templates.
- */
-public class Node {
+class Node implements Comparable<Node>{
+    Integer id;
+    NodeColor color;
+    Integer distance;
+    Node parent;
+
+    public Node(Integer id) {
+        this.id = id;
+    }
+
+    public int compareTo(Node otherNode) {
+        return this.id.compareTo(otherNode.id);
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (object instanceof Node) {
+            Node otherNode = (Node)object;
+            if (this.id == otherNode.id) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "" + id;
+    }
+}
+
+enum NodeColor {
+    WHITE,
+    GREY,
+    BLACK
 }
